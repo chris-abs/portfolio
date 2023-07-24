3b51c3b474181646bd3848e59b51844ab39e121d
Chris Abbott <chrisabbott@chriss-mac-mini.home>
2023-07-24 12:01:17 +0100

    bug fixes

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,23 +1,23 @@
-import { IconType } from 'react-icons';
+import { IconType } from 'react-icons'
 
 interface WidgetProps {
-	icon: IconType;
-	label: string;
-	description?: string;
+  icon: IconType
+  label: string
+  description?: string
 }
 
 const Widget: React.FC<WidgetProps> = ({ icon: Icon, label, description }) => {
-	return (
-		<div className="flex flex-col self-start text-center max-w-[280px] break-all  ">
-			{Icon && <Icon size={60} className="m-auto" />}
-			<div className="flex flex-row items-center gap-4">
-				<div className="flex flex-col">
-					<div className="text-lg font-semibold">{label}</div>
-					<div className=" text-description">{description}</div>
-				</div>
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className='flex flex-col self-start text-center max-w-[280px]'>
+      {Icon && <Icon size={60} className='m-auto' />}
+      <div className='flex flex-row items-center gap-4'>
+        <div className='flex flex-col'>
+          <div className='text-lg font-semibold'>{label}</div>
+          <div className='text-description'>{description}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Widget;
+export default Widget
--- src/features/LandingPage/sections/Home/index.tsx
+++ src/features/LandingPage/sections/Home/index.tsx
@@ -1,51 +1,47 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 const HomeSection = () => {
-	return (
-		<div>
-			<main>
-				<section id="home" className="min-h-screen overflow-x-hidden">
-					<div className="flex flex-col items-center justify-center h-screen z-1">
-						<h2 className="heading text-4xl lg:text-5xl xl:text-6xl md:py-1 lg:py-2 z-10 ">
-							Chris Abbott
-						</h2>
-						<Image
-							priority
-							fill
-							src="/belossom.jpg"
-							alt="belossom tree"
-							className="object-cover"
-						/>
-						<div className="overflow-x-none background z-10 relative">
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-						</div>
-						<h3 className="heading text-lg md:text-xl lg:text-2xl xl:text-3xl z-10">
-							I build things for the web.
-						</h3>
-					</div>
-				</section>
-			</main>
-		</div>
-	);
-};
+  return (
+    <section id='home' className='overflow-x-hidden'>
+      <div className='flex flex-col items-center justify-center h-screen z-1'>
+        <h1 className='heading text-4xl lg:text-5xl xl:text-6xl md:py-1 lg:py-2 z-10 '>
+          Chris Abbott
+        </h1>
+        <Image
+          priority
+          fill
+          src='/belossom.jpg'
+          alt='belossom tree'
+          className='object-cover'
+        />
+        <div className='overflow-x-none background z-10 relative'>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <h2 className='heading text-lg md:text-xl lg:text-2xl xl:text-3xl z-10'>
+          I build things for the web.
+        </h2>
+      </div>
+    </section>
+  )
+}
 
-export default HomeSection;
+export default HomeSection
